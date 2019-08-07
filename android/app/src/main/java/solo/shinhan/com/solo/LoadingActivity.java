3591cbfe24e997c4cdf268d7e56ea90b6ef9fbcc
kang <java7ang@gmail.com>
2019-08-08 01:21:00 +0900

    solo 로딩화면 만들기
    * solo 로딩화면을 만들기
    * 현재까지는 LoadingActivity 에서 MainActivity로 넘어가도록만 구성
    
    ref #7

---
+++ android/app/src/main/java/solo/shinhan/com/solo/LoadingActivity.java
@@ -0,0 +1,29 @@
+package solo.shinhan.com.solo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class LoadingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        startLoading();
+    }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+}
