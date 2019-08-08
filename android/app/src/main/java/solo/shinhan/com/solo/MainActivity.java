fe3b0abf8c24766a67f594510debe01a2410c0b6
kang <java7ang@gmail.com>
2019-08-09 01:34:50 +0900

    메인 화면개발1
    * 검색 창 개발
    * 설정 버튼 이미지 넣기
    
    ref #12

--- android/app/src/main/java/solo/shinhan/com/solo/MainActivity.java
+++ android/app/src/main/java/solo/shinhan/com/solo/MainActivity.java
@@ -1,10 +1,11 @@
 package solo.shinhan.com.solo;
 
+import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
