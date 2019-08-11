6f9b6ab2c74c9cbb65b1c62911748fe8aeb2982f
kang <java7ang@gmail.com>
2019-08-11 16:12:06 +0900

    집 세부 화면 구축하기.
    * 집 세부 화면 열기
    * 메인 리스트에 있는 각 집의 정보를 뿌려줘야함.
    
    ref #19

--- android/app/src/main/java/solo/shinhan/com/solo/MainActivity.java
+++ android/app/src/main/java/solo/shinhan/com/solo/MainActivity.java
@@ -1,10 +1,13 @@
 package solo.shinhan.com.solo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -31,5 +34,16 @@ public class MainActivity extends Activity {
         mHouseList = (ListView) findViewById(R.id.house_list_view);
 
         mHouseList.setAdapter(mListAdapter);
+
+        mHouseList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getBaseContext(), HouseDetailActivity.class);
+                intent.putExtra("houseNo",i);
+                startActivity(intent);
+                overridePendingTransition(0,0);
+
+            }
+        });
     }
 }
